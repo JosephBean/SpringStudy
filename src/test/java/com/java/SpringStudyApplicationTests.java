21c4da8bdbb92ead5f1ecd48a8d3702bf09b4d6c
JosephBean <kk1593@naver.com>
2024-12-27 08:57:07 +0900

    Spring Project

---
+++ src/test/java/com/java/SpringStudyApplicationTests.java
@@ -0,0 +1,14 @@
+package com.java;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest @Disabled
+class SpringStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
