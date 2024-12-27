6a8211bcfa47081945e4ae42b3d2998fae60e262
JosephBean <kk1593@naver.com>
2024-12-27 13:19:04 +0900

    Request Annotation

--- src/main/java/com/java/controller/ViewController.java
+++ src/main/java/com/java/controller/ViewController.java
@@ -2,10 +2,11 @@ package com.java.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/view")
 @Controller
@@ -16,15 +17,16 @@ public class ViewController {
 		return "page1";
 	}
 	
-	@PostMapping(value = "/page2")
-	public String page2() {
+	@PostMapping(value = "/page2/{test}")
+	public String page2(@PathVariable(name = "test") int name) {
+		System.out.println(name);
 		return "page2";
 	}
 	
-	@GetMapping(value = "/page3")
-	public String page3() {
+	@GetMapping(value = {"/page3/{age:[0-9]+}", "/page3"})
+	public String page3(@PathVariable(name = "age", required = false) Integer name) {
+		System.out.println(name);
 		return "page3";
 	}
-	
 	
 }
